1f410b371f80af3cf0d99309c57fc2eea801694c
denzel96 <daniel.szabo@attentioncrm.com>
2020-12-04 13:18:46 +0100

    second day

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        new Day1().getTask1Answer();
-        System.out.println(new Day1().getTask2Answer());
+        System.out.println(new Day2().getTask1Answer());
     }
 }
