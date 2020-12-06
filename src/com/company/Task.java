f0e2cbd9bddb7acf73122e4b38e562d100fe50b3
Dániel Szabó <szabo.daniel1996@gmail.com>
2020-12-06 02:48:09 +0100

    interface -> abstract class

--- src/com/company/Task.java
+++ src/com/company/Task.java
@@ -3,8 +3,42 @@ package com.company;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-public interface Task {
-    Object getTask1Answer();
-    Object getTask2Answer();
+public abstract class Task {
+    abstract void parseInput();
+
+    abstract Object getTask1Answer();
+
+    abstract Object getTask2Answer();
+
+    public Task() {
+        parseInput();
+    }
+
+    static List<String> getInput(String fileName) {
+        List<String> result = null;
+        try (Stream<String> lines = Files.lines(Paths.get("src/com/inputs/" + fileName))) {
+            result = lines.collect(Collectors.toList());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+
+    void showTask1Answer() {
+        System.out.println(getTask1Answer());
+    }
+
+    void showTask2Answer() {
+        System.out.println(getTask2Answer());
+    }
+
+
 }
